28557db723f7404ff8076da5282216f83030a171
MezeiSebastian <61548741+MezeiSebastian@users.noreply.github.com>
2020-10-15 13:01:57 +0300

    added submit cancel and menu functionality and moved inline css to css files

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import 'com.training.physical-examination/dist/index.css'
 import  PhysicalExaminationComponent  from 'com.training.physical-examination'
 import {IntlProvider} from 'react-intl';
 const App = () => {
--- src/components/action-footer-component/action-footer.component.tsx
+++ src/components/action-footer-component/action-footer.component.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import BlockActionFooter from 'terra-action-footer/lib/BlockActionFooter'
 import Spacer from 'terra-spacer';
 import Button from 'terra-button';
-class PhysicalExaminationFooter extends React.Component<{onSubmit:()=>void}>{
+import styles from './action-footer.module.css'
+
+class PhysicalExaminationFooter extends React.Component<{onSubmit:()=>void,onCancel:()=>void}>{
 
   constructor(props) {
     super(props)
     this.submitHandler = this.submitHandler.bind(this);
+    this.cancelHandler = this.cancelHandler.bind(this);
    
   }
 
@@ -15,30 +18,22 @@ class PhysicalExaminationFooter extends React.Component<{onSubmit:()=>void}>{
     this.props.onSubmit();
   }
 
+  cancelHandler(event){
+    event.preventDefault();
+    this.props.onCancel();
+  }
+
   render(){
     return(
-      <div style={{bottom:0}}>
-      <BlockActionFooter>
-      <div
-        style={{
-          backgroundColor: '#e5eef9',
-          left: '0',
-          bottom: '0',
-          position: 'fixed',
-          width: '100%',
-          height: '10%',
-          borderRadius: '0px 0px 25px 25px',
-          borderTopStyle: 'solid',
-          borderTopColor: '#c8def5',
-          minHeight:'70px'
-        }}
-      >
-        <div style={{float: 'right', marginTop: '25px', marginRight: '15px'}}>
+      <div className={styles.bottom}>
+      <BlockActionFooter >
+      <div className={styles.footerDiv}>
+        <div className={styles.buttons}>
         <React.Fragment>
           <Spacer isInlineBlock marginRight='medium'>
             <Button text='Submit' variant={Button.Opts.Variants.EMPHASIS} onClick= {this.props.onSubmit}/>
           </Spacer>
-          <Button text='Cancel' />
+          <Button text='Cancel' onClick= {this.props.onCancel}/>
         </React.Fragment>
         </div>
       </div>
@@ -51,3 +46,4 @@ class PhysicalExaminationFooter extends React.Component<{onSubmit:()=>void}>{
 
 }
 export default PhysicalExaminationFooter
+
